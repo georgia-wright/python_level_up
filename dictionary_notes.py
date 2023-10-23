9d4aa28e5a0c651afc5471390b1e8c691aaffe69
Georgia Wright <georgiawright617@gmail.com>
2023-10-23 17:28:22 +0100

    end of day 1 week 5

--- dictionary_notes.py
+++ dictionary_notes.py
@@ -1,1 +1,64 @@
-print("this is dictionaries")
+# print("this is dictionaries")
+
+# capital_cities = {
+#     "England" : "London",
+#     "Scotland" : "Edinburgh",
+#     "Wales" : "Cardiff",
+#     "Northern Ireland" : "Belfast",
+#     "Ireland" : "Dublin"
+# }
+
+# print(capital_cities)
+
+# print(capital_cities["England"])
+
+
+# activity 1
+
+animals = {
+    "Lion" : "Cub",
+    "Dog" : "Puppy",
+    "Cat" : "Kitten",
+    "Chicken" : "Chick"
+}
+
+print(animals["Cat"])
+
+animals["Cat"] = "Kitty"
+
+print(animals)
+
+print(animals.keys())
+
+print(animals.values())
+
+print(animals.items())
+
+print(animals.get("Dog"))
+
+print(animals["Dog"])
+
+print(animals.get("Dragon"))
+
+# print(animals["Dragon"]) - gives error
+
+print(animals.get("Dragon", "We couldn't find a baby animal for that adult animal."))
+
+
+(animals.setdefault("Pig", "Piglet"))
+
+print(animals)
+
+animals.pop("Lion")
+
+print(animals)
+
+
+
+
+
+
+
+
+
+
