80218b5a4e32cba5c7be0e4d21c02cd02f95a44a
Georgia Wright <georgiawright617@gmail.com>
2023-10-25 09:11:50 +0100

    2 challenges dictionary

---
+++ challenge_1.py
@@ -0,0 +1,12 @@
+favourite_songs = {
+    "The Wombats" : "Greek Tragedy",
+    "Foo Fighters" : "Best of You",
+    "Prince" : "Kiss",
+    "Fleetwood Mac" : "Silver Springs",
+    "The 1975" : "You"
+}
+
+favourite_songs.update({"Christine and the Queens" : "It", "Empire of the Sun" : "We Are the People"})
+
+print(favourite_songs)
+
---
+++ challenge_2.py
@@ -0,0 +1,20 @@
+
+countries = [
+    "England",
+    "Spain",
+    "Ethiopia",
+    "Iran"
+]
+
+languages = [
+    "English",
+    "Spanish",
+    "Amharic",
+    "Farsi"
+]
+
+
+dictionary = dict(zip(countries, languages))
+
+print(dictionary)
+
